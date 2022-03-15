61967acc23e3eb2b0ef0e71179f9825e3932aa15
Kalimeris Spyros <kalimerisspyros@gmail.com>
2022-03-15 12:26:07 +0200

    Started to working with mongoose and create a first simple Schema

--- server.js
+++ server.js
@@ -1,7 +1,56 @@
-const dotenv = require('dotenv');
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(
+  DB,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log(" Database is running Mongoose is connected");
+  }
+);
+
+// To create a model we need  a Schema
+
+// Basic way of describing the data types and the Schema
+// Scema type options
+
+// const toursScema = new mongoose.Schema({
+//   name: String,
+//   rating: Number,
+//   price: Number,
+// });
+
+const toursSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+// letd create a model our of our schema
+
+// use uppercase on models
+const Tour = mongoose.model("Tour", toursSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
