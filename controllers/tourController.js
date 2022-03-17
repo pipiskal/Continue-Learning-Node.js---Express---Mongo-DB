af6f0901b8453babbaf196f2beba30736725e513
Kalimeris Spyros <kalimerisspyros@gmail.com>
2022-03-17 09:42:12 +0200

    undestanidng the find() method

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,36 +1,53 @@
 const Tour = require("./../models/tourModel");
 
-// const tours = JSON.parse(
-//   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-// );
-
-// exports.checkID = (req, res, next, val) => {
-//   console.log(`Tour id is: ${val}`);
-
-//   if (req.params.id * 1 > tours.length) {
-//     return res.status(404).json({
-//       status: "fail",
-//       message: "Invalid ID",
-//     });
-//   }
-//   next();
-// };
-
-// exports.checkBody = (req, res, next) => {
-//   if (!req.body.name || !req.body.price) {
-//     return res.status(400).json({
-//       status: "fail",
-//       message: "Missing name or price",
-//     });
-//   }
-//   next();
-// };
-
 exports.getAllTours = async (req, res) => {
   // since we dont pass anything inside find(),it will bring all the documents back
   // from the specific model --> collection
-  const tours = await Tour.find();
   try {
+    // BUILD QUERY
+    // 1)Filtering
+    // create a hardcopy of the req.obj so we wont pollute the filtering options
+    let queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+
+    // deleting the exluded fields from the main req.query
+    excludedFields.forEach((el) => {
+      delete queryObj[el];
+    });
+
+    // req.params --> {name : "spyros"} : "/:name?" ? => optional;
+    // req.query -->  {duration : "5", difficulty : "easy"}
+
+    // mongoDb query = { duration : 5, difficulty : "easy" } --> its the exact same with the req.query
+
+    // 1A) Simple Filtering
+
+    // 1st way to query the database with the query information
+    // it seems that the find method will ignore the  query parameters
+    // that doenst match the schema
+
+    // when we use await the result of the query the query will execute give the results
+    // So if we execute the query we wont be able to chain other methods, so we have to return a query
+    // and when we are done with filtering we can execute the query by awaiting
+
+    // How the filter object would look like if we had to check for less than or greater than
+    // {difficulty : "easy" , duration : {$gte : 5}}
+    // { difficulty: 'easy', duration: { gte: '5' } } --> queryObject
+    // so what we have to do is to replace gte or lte or gt or lt with $gtw and etc
+
+    // 2) Advanced Filtering
+    console.log(queryObj);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    queryObj = JSON.parse(queryStr);
+    console.log(queryObj);
+
+    const query = Tour.find(queryObj);
+
+    // 2) EXECUTE QUERY
+    const tours = await query;
+
+    // 3) SEND RESPONSE
     res.status(200).json({
       status: "success",
       requestedAt: req.requestTime,
@@ -42,7 +59,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: "fail",
-      message: err,
+      message: err.message,
     });
   }
 };
@@ -91,6 +108,8 @@ exports.createTour = async (req, res) => {
 
 exports.updateTour = async (req, res) => {
   try {
+    console.log(req.params.id);
+    console.log(req.body);
     const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true, // the new updated document will be returned
       runValidators: true,
--- server.js
+++ server.js
@@ -2,18 +2,16 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
 
-// dotenv.config({ path: "./config.env" });
+dotenv.config({ path: "./config.env" });
 
-// const DB = process.env.DATABASE_LOCAL;
+const DB = process.env.DATABASE_LOCAL;
 
-// console.log(DB);
-
-mongoose.connect("mongodb://localhost:27017/natours", (err) => {
+mongoose.connect(DB, (err) => {
   if (!err) console.log("db connected");
   else console.log("db error");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${process.env.PORT}...`);
 });
